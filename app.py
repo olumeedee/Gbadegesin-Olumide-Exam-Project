c5232a6bbc2e8368b1e86c4ba675ec2beb46af68
Olumide Gbadegesin <olumeedee@gmail.com>
2022-11-02 17:28:42 +0100

    Basic Html templates

--- app.py
+++ app.py
@@ -1,23 +1,38 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Olumide Gbadegesin',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'November 2, 2022'
+    },
+    {
+        'author': 'Bruce Wayne',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'November 2, 2022'
+    }
+]
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return "Hello world!"
+    return render_template('index.html', posts=posts)
 
 @app.route('/about')
 def about():
-    return "The about page"
+    return render_template('about.html', title='About')
 
 @app.route('/contact')
 def contact():
-    return "The contact page"
+    return render_template('contact.html', title='Contact Me')
 
 @app.route('/protected')
 def protected():
     return "The protected page"
-    
+
 if __name__ == "__main__":
     app.run(debug=True)
